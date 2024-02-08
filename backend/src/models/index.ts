a00b48a0d5bdab325782a715342caa1208577d1e
unknown <josealvarez1654@outlook.com>
2024-02-08 13:22:43 -0500

    transpilacion del config.json

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const _process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/config.json')[env];
+const config = require('../../config/config.json');
 const db:any = {};
 // import dotenv from 'dotenv';
 
@@ -15,7 +15,7 @@ let sequelize: any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {host:config.development.host, dialect:config.development.dialect});
 }
 
 fs
