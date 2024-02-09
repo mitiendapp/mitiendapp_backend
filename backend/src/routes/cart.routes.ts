dfed01259a84d14de7d9e39931b48667cda97077
count <santiagoosorio310@gmail.com>
2024-02-09 13:23:14 -0500

    hu12(feature): create client routes

---
+++ backend/src/routes/cart.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getProducts } from "../controllers/product.controller";
+import { addProducts, removeProducts } from "../controllers/shoppingcart.controller";
+
+const ROUTER = Router();
+
+ROUTER.get('/cart/get', getProducts); // pendiente de cambiar
+ROUTER.post('/cart/add', addProducts); // pendiente de cambiar
+//ROUTER.post('/cart/remove', removeProducts)
+
+module.exports = ROUTER;
