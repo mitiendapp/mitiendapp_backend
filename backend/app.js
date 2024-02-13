441b2e929f2f9174068254f6b65837dd0f52113b
unknown <josealvarez1654@outlook.com>
2024-02-13 01:08:41 -0500

    prueba de despligue 8.0

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 var app = express()
   .use(cors({
-    credentials: true, origin: ['http://localhost:4200','https://pruebabackend1.onrender.com']
+    credentials: true, origin: 'http://localhost:4200'
   }))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }));
