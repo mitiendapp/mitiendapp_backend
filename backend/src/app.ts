2c878b0abda3ce1897e8ee59feffb97b69716010
unknown <josealvarez1654@outlook.com>
2024-02-14 11:18:52 -0500

    prueba desploigue sena1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,7 +29,7 @@ app.use(morgan((tokens, req, res) => {
     ].join(' ');
   }));
 
-const allowedOrigins = ['http://localhost:4200', 'http://127.0.0.1:4040', 'https://d43d-152-202-200-21.ngrok.io','https://mitiendapp2.netlify.app/']
+const allowedOrigins = ['http://localhost:4200', 'http://127.0.0.1:4040', 'https://d43d-152-202-200-21.ngrok.io','https://mitiendapp2.netlify.app']
 app.use(cors({
     credentials: true,
     origin: allowedOrigins
