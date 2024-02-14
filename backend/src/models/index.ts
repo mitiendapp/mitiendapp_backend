df8e9c12f4a3ba715d386fe9e4c3fb0fc49e023f
jose leonardo alvarez hoyos <110494963+joseleonardoalvarezhoyos21@users.noreply.github.com>
2024-02-14 08:44:07 -0500

    Update index.ts
    se agrego conexión a la base de datos cambiando el index.tx y el config.json

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -6,14 +6,14 @@ const Sequelize = require('sequelize');
 const _process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/config.json')[env];
+const config = require(../../config/config.json');
 const db:any = {};
 
 let sequelize: any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.production.database, config.production.username, config.production.password, {host:config.production.host, dialect:config.production.dialect});
 }
 
 fs
