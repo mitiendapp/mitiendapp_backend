977838122a2316780c7214d3a4994217a02995c2
unknown <josealvarez1654@outlook.com>
2024-03-03 17:06:58 -0500

    cambio imagen correo

--- backend/src/controllers/correo.controller.ts
+++ backend/src/controllers/correo.controller.ts
@@ -20,7 +20,7 @@ export const envioCorreo = (req: Request, resp: Response) => {
         to: body.email,
         subject: 'Mitiendapp_Bienvenida',
         text: 'Beinvenido a mitiendapp',
-        html: '<img src="../src/assets/img/bienvenidaClientes.png" alt="Imagen adjunta">',
+        html: '<img src="../../src/assets/img/bienvenidaClientes.png" alt="Imagen adjunta">',
         attachments: [
             {
               filename: 'imagen.jpg',
