cc4af4dea311da2d616031d9a3434cbf2610e4d8
unknown <josealvarez1654@outlook.com>
2024-03-03 19:55:50 -0500

    correoCompany

--- backend/src/controllers/correo.controller.ts
+++ backend/src/controllers/correo.controller.ts
@@ -1,3 +1,4 @@
+import { text } from 'body-parser';
 import express, { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 
@@ -16,19 +17,20 @@ export const envioCorreo = (req: Request, resp: Response) => {
     });
 
     const opciones = {
+        
         from: 'mitiendapp6@gmail.com',
         to: body.email,
         subject: 'Mitiendapp_Bienvenida',
-        text: 'Mitiendapp_Bienvenida',
-        html: '<img src="https://lh3.googleusercontent.com/pw/AP1GczOjTf8fDq0R9rQzzrJqLXOVKE8Pw9iKKe0v02hmNcIySqPOcLuPF7SoNLUSVql_MIwtL90QFplunOjKKXanRmO7MJjZAmuxm-O_9ZLmoboDNgkPflRLZtxLCj-_5qvmCeIEWCiRjSPJpLJ8kswUtrnRLiAauXTg64ew5aze2TRxYdjQwRE3t4-PNHOFN4J_GuGg7s4INui4XTm6iu3ymSNY9X692eyql-i4OWo8WIrr0-axC4bdht12YZJ6eOtRvFiPZQXAEnqMEHQ8frBtTwKZSzVyQkWETMUpmsQKi5huD6o7VycZCJpAXG-6z-nxNMfnwwO7nAKXI5ujMV7vBk5EWuYvj4NsyJN-L3EghOvUffmqprG_ecl1aqUKOdVTsao0TtwrHnH_MR2JcJPbEusxwZD_H6j1IK2aWrgm0IQzrdtibxYCbWRUbneUFuBgUjx4pLLyal6sgOrPkImdmLq1E0B6oWKavAWOl2HuifhFa4k3xB6IyJ6Bm7XwgX6NJLqq0RdATGQCp1Ba_HZvOTdsa5dL4KLbwPer3yvirjrag1vJG4s0ZuzFtiepqYkGtAlKOIJ6YE_Dojx1zJPWigMqWNhEgTqKHxhc4CG6Kou8RH22t2ejyAQaQ5WD3Np-2NMCHJd1ButKCbKbfnpIKw2NRSIwV38J8dw_T-UA2S9iGTQuXlIJTCgXpxwWEWBwELoCTVf2xhalM0GP-sl0M-JBQK8kuLxhGiR6DQQlBLb5otSw5tT9XfxtiVaWMryvjy-G-p0Rt-VuRpfkyQwGRsSwEFrl71Kg0xfU1RcTVAEk1kiUzeqnDKRC8CRRCoxC-q8GDYbiO2lENPISkk-PUHR4zPB2SKVp0kFAN-IG3DUmlXQKnjjEr_Rfkli58M42TfWV6b5FXu0VV1MlVGbuShd3=w599-h599-s-no-gm?authuser=0" alt="Imagen adjunta">',
-        attachments: [
-            {
-              filename: 'imagen.jpg',
-              path: '',
-              cid: 'Bienvenido', 
-            },
-          ],
-    };
+        html:  '<HTML><br><img src="https://lh3.googleusercontent.com/pw/AP1GczOjTf8fDq0R9rQzzrJqLXOVKE8Pw9iKKe0v02hmNcIySqPOcLuPF7SoNLUSVql_MIwtL90QFplunOjKKXanRmO7MJjZAmuxm-O_9ZLmoboDNgkPflRLZtxLCj-_5qvmCeIEWCiRjSPJpLJ8kswUtrnRLiAauXTg64ew5aze2TRxYdjQwRE3t4-PNHOFN4J_GuGg7s4INui4XTm6iu3ymSNY9X692eyql-i4OWo8WIrr0-axC4bdht12YZJ6eOtRvFiPZQXAEnqMEHQ8frBtTwKZSzVyQkWETMUpmsQKi5huD6o7VycZCJpAXG-6z-nxNMfnwwO7nAKXI5ujMV7vBk5EWuYvj4NsyJN-L3EghOvUffmqprG_ecl1aqUKOdVTsao0TtwrHnH_MR2JcJPbEusxwZD_H6j1IK2aWrgm0IQzrdtibxYCbWRUbneUFuBgUjx4pLLyal6sgOrPkImdmLq1E0B6oWKavAWOl2HuifhFa4k3xB6IyJ6Bm7XwgX6NJLqq0RdATGQCp1Ba_HZvOTdsa5dL4KLbwPer3yvirjrag1vJG4s0ZuzFtiepqYkGtAlKOIJ6YE_Dojx1zJPWigMqWNhEgTqKHxhc4CG6Kou8RH22t2ejyAQaQ5WD3Np-2NMCHJd1ButKCbKbfnpIKw2NRSIwV38J8dw_T-UA2S9iGTQuXlIJTCgXpxwWEWBwELoCTVf2xhalM0GP-sl0M-JBQK8kuLxhGiR6DQQlBLb5otSw5tT9XfxtiVaWMryvjy-G-p0Rt-VuRpfkyQwGRsSwEFrl71Kg0xfU1RcTVAEk1kiUzeqnDKRC8CRRCoxC-q8GDYbiO2lENPISkk-PUHR4zPB2SKVp0kFAN-IG3DUmlXQKnjjEr_Rfkli58M42TfWV6b5FXu0VV1MlVGbuShd3=w599-h599-s-no-gm?authuser=0" alt="Imagen adjunta"></br></HTML>',
+        text: 'Bienvenido ' + body.email,
+    //     attachments: [
+    //         {
+    //           filename: 'imagen.jpg',
+    //           path: '',
+    //           cid: 'Bienvenido', 
+    //         },
+    //       ],
+     };
 
     config.sendMail(opciones, function(error, result) {
         if (error) return resp.json({ ok: false, msg: error });
--- backend/src/controllers/correoCompany.controller.ts
+++ backend/src/controllers/correoCompany.controller.ts
@@ -20,14 +20,14 @@ export const envioCorreoCompany = (req: Request, resp: Response) => {
         to: body.email,
         subject: 'Pruebas Correo',
         text: 'Beinvenido a mitiendapp',
-        html: '<p><img src="https://lh3.googleusercontent.com/pw/AP1GczOjTf8fDq0R9rQzzrJqLXOVKE8Pw9iKKe0v02hmNcIySqPOcLuPF7SoNLUSVql_MIwtL90QFplunOjKKXanRmO7MJjZAmuxm-O_9ZLmoboDNgkPflRLZtxLCj-_5qvmCeIEWCiRjSPJpLJ8kswUtrnRLiAauXTg64ew5aze2TRxYdjQwRE3t4-PNHOFN4J_GuGg7s4INui4XTm6iu3ymSNY9X692eyql-i4OWo8WIrr0-axC4bdht12YZJ6eOtRvFiPZQXAEnqMEHQ8frBtTwKZSzVyQkWETMUpmsQKi5huD6o7VycZCJpAXG-6z-nxNMfnwwO7nAKXI5ujMV7vBk5EWuYvj4NsyJN-L3EghOvUffmqprG_ecl1aqUKOdVTsao0TtwrHnH_MR2JcJPbEusxwZD_H6j1IK2aWrgm0IQzrdtibxYCbWRUbneUFuBgUjx4pLLyal6sgOrPkImdmLq1E0B6oWKavAWOl2HuifhFa4k3xB6IyJ6Bm7XwgX6NJLqq0RdATGQCp1Ba_HZvOTdsa5dL4KLbwPer3yvirjrag1vJG4s0ZuzFtiepqYkGtAlKOIJ6YE_Dojx1zJPWigMqWNhEgTqKHxhc4CG6Kou8RH22t2ejyAQaQ5WD3Np-2NMCHJd1ButKCbKbfnpIKw2NRSIwV38J8dw_T-UA2S9iGTQuXlIJTCgXpxwWEWBwELoCTVf2xhalM0GP-sl0M-JBQK8kuLxhGiR6DQQlBLb5otSw5tT9XfxtiVaWMryvjy-G-p0Rt-VuRpfkyQwGRsSwEFrl71Kg0xfU1RcTVAEk1kiUzeqnDKRC8CRRCoxC-q8GDYbiO2lENPISkk-PUHR4zPB2SKVp0kFAN-IG3DUmlXQKnjjEr_Rfkli58M42TfWV6b5FXu0VV1MlVGbuShd3=w599-h599-s-no-gm?authuser=0"></p>',
-        attachments: [
-            {
-              filename: 'imagen.jpg',
-              path: '',
-              cid: 'Bienvenido', 
-            },
-          ],
+        html: '<p><img src="https://lh3.googleusercontent.com/pw/AP1GczP2Yf1zciLiIoLVfhWM-4-SY5lyzH8OCFsbY2soJn3GbeQBPC31DOVtPK5NRZ46rSb0G1iy4a_Wvhbg5N9fW27WnF7u18Ike8ZRzwZD9KsZDFRYaa5hWhEY7slnN2Iqpptgyz7o2qL9OuuYdfpSIWnBioyrXFatgluMU0hSiU5TEceR5-x5htZ_iy11TQh6xdAZZK1ckTkyRlWwITXNtcqMRa5mXij_KGiiQ9sZ3-5r4RDGdirunkR2pyQt6nrdPfKWzsJ46JRLYHftVELz7MLXcQyXXxi_bsEioc9v5XMTxQUHl37gstyxxbCaREJQKDpAZqPDGlY4BOmzflIORU1-F2Bb7ciK1VClfbfSPJ6GQRBSQKzzlYcEqsBfvY9oSkV28DmjA6EJQmO3AVTWncQ1ooYK4KOTo29yA561hiB1FJSFUIFm9aFbJ4hYxXiutMxteW7lZW7UCmyoWoGvZan1MrQ3HN38Ee8mMJtko9vxf2320h54xe6v1e0iiOUStLmSosu0ZZJkRLeBVjXHREvFSTU2nV_AFM_KILHdwv5vUbfxO9X10Iuzo40VHof2hPCcN-wI35AI6uSzanwczqhA7o2LIsGzawdatrEcObBOt7uz8cz_MqMxlqjhFV-3x-xqgiwPIEayQf4ltHhqruUuzJy4TEg7FEaNUdk7GhY097JsokJ62h5dlfGTpeCOnoZKJs5kiDi06OgFd91DFxe_C_tLjy2vC2yGn0Paym82yyq4_OhANeKLeaF33JQYnR2b0und2I_Co5eXlZo8GJfmwTWU3twJgBXfAHEU7b00UBe43oo1BbU7AMQZzOWTr_s0rjBTvP0sB8Ympha1rDCDqhQz4EfBRpXpIr2_6K90Hrx34orMXuQK3HS8qSbuXRKtgngPpZvivtCpZF2Y5Z8=w337-h599-s-no-gm?authuser=0"></p>',
+       // attachments: [
+            // {
+            //   filename: 'imagen.jpg',
+            //   path: '',
+            //   cid: 'Bienvenido', 
+            // },
+         // ],
     };
 
     config.sendMail(opciones, function(error, result) {
