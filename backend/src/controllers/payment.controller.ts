f75aa0b23eab07f5875e804b4d7e7ba702249a0c
count <santiagoosorio310@gmail.com>
2024-03-03 20:17:29 -0500

    payment controller update

--- backend/src/controllers/payment.controller.ts
+++ backend/src/controllers/payment.controller.ts
@@ -1,20 +1,31 @@
 import e, { NextFunction, Request, Response } from "express";
 import mercadopago from 'mercadopago';
+import { PreferenceItem, PreferencePayer } from "mercadopago/models/preferences/create-payload.model";
 let url= "https://a797-2801-1ca-5-511-9dd6-6fad-51ac-59f.ngrok.io"
 export const createOrder = async(req:Request,res:Response,next:NextFunction)=>{
+    console.log(req.body);
+    let prefItem:PreferenceItem;
+    let prefPayer:PreferencePayer;
+
+    prefItem ={
+        category_id:undefined,
+        currency_id:undefined,
+        description:req.body.description,
+        id:req.body.id,
+        picture_url:req.body.image,
+        quantity:req.body.quantity,
+        title:req.body.name,
+        unit_price:req.body.price
+    }
+    prefPayer = {
+        name:req.body
+    }
     try{
     mercadopago.configure({
         access_token:"TEST-5979417188444398-062800-d2d07c1dea382c16ce5091b6cd5a8f3b-1409740750"
     });
-    console.log("entra 1");
     const result = await mercadopago.preferences.create({
-        items:[
-            {
-                title:"telefono",
-                unit_price: 1000,
-                currency_id: "COP",
-                quantity:12
-        }],
+        items:[prefItem],
         back_urls:{
             success: "http://localhost:4200/",
             failure: "http://localhost:3000/api/order/failure",
--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -4,16 +4,16 @@ import { createOrder, receiveWebhook } from "../controllers/payment.controller";
 
 const ROUTER = Router();
 
-ROUTER.post('/order/create', verifyJWT , createOrder); // pendiente de cambiar
+ROUTER.post('/order/create', verifyJWT, createOrder); // pendiente de cambiar
 
 //ROUTER.get('/order/success', orderSuccess);
-ROUTER.get('/order/failure', (req,res)=>{ // pendiente de cambiar
+ROUTER.get('/order/failure', (req, res) => { // pendiente de cambiar
     console.log("orden creada");
 })
 
-ROUTER.get('/order/pending', (req,res)=>{ // pendiente de cambiar
+ROUTER.get('/order/pending', (req, res) => { // pendiente de cambiar
     console.log("orden creada");
-    
+
 })
 
 ROUTER.post('/order/webhook', receiveWebhook) // pendiente de cambiar
