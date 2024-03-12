1a1ce32846bf2fa8e39d8e20a5c48d51f4c3c8de
count <santiagoosorio310@gmail.com>
2024-03-11 19:44:30 -0500

    Revert "bakend"
    This reverts commit d744dbe97a4c2b29b9a99662f44972c8bbbd31b1.

--- backend/src/controllers/company.controller.ts
+++ backend/src/controllers/company.controller.ts
@@ -66,10 +66,10 @@ export const updateCompany: RequestHandler = async (
 ) => {
     const companyService = new CompanyService();
     try {
-        const {companyId} = req.query;
-        console.log(companyId);
+        const {email} = req.query;
+        console.log(email);
         
-        const updatedCompany = await companyService.update(companyId, req.body );
+        const updatedCompany = await companyService.update(email, req.body );
         return res.status(201).json({
             message: "Datos actualizados correctamente",
             data: updatedCompany
