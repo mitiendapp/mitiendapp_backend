d87290a970554dbf08ab87452fb9e6ad4d67638d
count <santiagoosorio310@gmail.com>
2024-03-14 16:54:52 -0500

    deploy

--- backend/build/src/models/index.js
+++ backend/build/src/models/index.js
@@ -18,15 +18,15 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 }
 else {
-    sequelize = new Sequelize(config.santiago.database, config.santiago.username, config.santiago.password, { host: config.santiago.host, dialect: config.santiago.dialect });
+    sequelize = new Sequelize(config.production.database, config.production.username, config.production.password, { host: config.production.host, dialect: config.production.dialect });
 }
 fs
     .readdirSync(__dirname)
     .filter((file) => {
     return (file.indexOf('.') !== 0 &&
         file !== basename &&
-        file.slice(-3) === '.ts' &&
-        file.indexOf('.test.ts') === -1);
+        file.slice(-3) === '.js' &&
+        file.indexOf('.test.js') === -1);
 })
     .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
